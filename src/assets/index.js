c21e7f0a3f35168c520b582fb30d7fb58ddb265f
Kuldeep Singh Bartwal <71578263+ksb44@users.noreply.github.com>
2024-07-06 19:53:14 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,7 +3,7 @@ import projectOne from "./images/projects/projectOne.jpg";
 import projectTwo from "./images/projects/projectTwo.jpg";
 import projectThree from "./images/projects/projectThree.jpeg";
 import projectFour from "./images/projects/projectFour.jpg";
-import projectFive from "./images/projects/projectFive.jpeg";
+import projectFive from "./images/projects/projectFive.jpg";
 import contactImg from "./images/contact/contactImg.png";
 
 export {
