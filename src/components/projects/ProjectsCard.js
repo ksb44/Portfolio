544c6034fefaa8791e98ab5db0b882963f692f6e
Kuldeep Singh Bartwal <71578263+ksb44@users.noreply.github.com>
2024-07-06 15:59:26 +0530

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { FaGlobe } from "react-icons/fa";
 
-const ProjectsCard = ({ title, des, src, link }) => {
+const ProjectsCard = ({ title, des, src, link ,web }) => {
   return (
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
       <div className="w-full h-[90%] overflow-hidden rounded-lg">
@@ -25,7 +25,9 @@ const ProjectsCard = ({ title, des, src, link }) => {
                 </a>
               </span>
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-white hover:text-purple-500 duration-300 cursor-pointer">
+             <a href={web} target="_blank" rel="noopener noreferrer">
                 <FaGlobe />
+            </a>
               </span>
             </div>
           </div>
