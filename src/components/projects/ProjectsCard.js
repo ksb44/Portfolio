67228ac1087b0097c183a8635e7d79dd957f221d
Kuldeep Singh Bartwal <71578263+ksb44@users.noreply.github.com>
2024-07-07 11:42:51 +0530

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -26,7 +26,8 @@ const ProjectsCard = ({ title, des, src, link ,web }) => {
               </span>
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-white hover:text-purple-500 duration-300 cursor-pointer">
              <a href={web} target="_blank" rel="noopener noreferrer">
-                <FaGlobe />
+                // <FaGlobe />
+            Live
             </a>
               </span>
             </div>
