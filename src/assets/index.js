42e9c02a0e4114cfb453cc1d6fd766d3e5508429
Kuldeep Singh Bartwal <71578263+ksb44@users.noreply.github.com>
2024-09-19 11:53:49 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,18 @@ import projectFour from "./images/projects/projectFour.jpg";
 import projectFive from "./images/projects/projectFive.jpg";
 import contactImg from "./images/contact/contactImg.png";
 
+const projectSix = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp5063343.jpg&f=1&nofb=1&ipt=4ae8de2ab9e71fb6ecbe723833f4418d269432451552e4b91b98b0a23eb08d03&ipo=images"
+const projectSeven = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuser-images.githubusercontent.com%2F69699279%2F163510440-72781f01-6627-4533-b899-6b3972138f4f.png&f=1&nofb=1&ipt=ed37875326582dbd3b6f9a68cb80cf347f1db9857d9e88e560d2db6ea72b69f5&ipo=images"
+const projectEight = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp5063343.jpg&f=1&nofb=1&ipt=4ae8de2ab9e71fb6ecbe723833f4418d269432451552e4b91b98b0a23eb08d03&ipo=images"
+const projectNine = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fis2-ssl.mzstatic.com%2Fimage%2Fthumb%2FPurple126%2Fv4%2Ffd%2F87%2F28%2Ffd8728f7-159a-e80b-52b2-1be20a7c4a89%2Fsource%2F512x512bb.jpg&f=1&nofb=1&ipt=37ee3fbaafd25fa49f0f07c31edce310d1406ceeead51053d03121bdf812a84b&ipo=images"
+const projectTen = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Frepository-images.githubusercontent.com%2F635720701%2F3ccfd205-3564-4a32-bd19-d3dc26053015&f=1&nofb=1&ipt=2e85fbe39ae8f60adaff1df9c9b9f7d47c155f1a14e4aee3d58fc4454fa69308&ipo=images"
+const projectEleven = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.scaler.com%2Ftopics%2Fimages%2Fviual-difference-between-listview-and-gridview.webp&f=1&nofb=1&ipt=6f7c322b187adee9af76add38c2704b82f77ff4013e8b4e4f669479fb2b38f00&ipo=images"
+const projectTwelve = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuploads-ssl.webflow.com%2F611ccf5afca6871db3be163a%2F611ccf5afca6875b47be168e_lh_bryntum-suite_schedulerpro_examples_grouping_%2520(1).png&f=1&nofb=1&ipt=5072f2ce3d6c955f9c305a3f654bdd56d5ac6b7382081e4c91728ad5fe222343&ipo=images"
+const projectThirteen = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs3.amazonaws.com%2Fdigitaltrends-uploads-prod%2F2016%2F01%2FNews-Apps-Header.jpg&f=1&nofb=1&ipt=edcfcc89cb28b7acf9ff7901632b1d78a931ae061029b599e0cdce20f3c12380&ipo=images"
+const projectFourteen = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcodesandbox.io%2Fapi%2Fv1%2Fsandboxes%2Fi56hi%2Fscreenshot.png&f=1&nofb=1&ipt=5e7aa2fd42516738c2073909245f0e66a8fa14161734d4886a0b21e4873c61a3&ipo=images"
+const projectFifteen = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.6Sg0OTlDtx9BPQZ3LR7SQwHaFM%26pid%3DApi&f=1&ipt=ed45c153764779fc9382057b375dad4ff9fd642e2836d560daa6e792e030a512&ipo=images"
+
+
 export {
 
   projectOne,
@@ -14,4 +26,14 @@ export {
   projectFour,
   projectFive,
   contactImg,
+  projectSix,
+   projectSeven,
+   projectEight,
+   projectNine,
+   projectTen,
+   projectEleven,
+   projectTwelve,
+   projectThirteen,
+   projectFourteen,
+  projectFifteen
 };
